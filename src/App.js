086f439f24b50686d2ceaab6db927a314abafc41
bethwel <bethwelifedha@gmail.com>
2023-03-10 09:43:58 +0300

    updated files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from "./logo.svg";
+
 import "./App.css";
-import { Container, Typography, Box, Stack, Grid, Button } from "@mui/material";
+import { Container, Typography, Box, Stack } from "@mui/material";
 import Appbar from "./components/appbar";
 import { ThemeProvider } from "@mui/system";
 import theme from "./styles/theme";
 import Banner from "./components/banner";
-import Products from "./components/products";
 import { UIProvider } from "./context/ui";
 import Footer from "./components/footer";
 import AppDrawer from "./components/drawer";
@@ -35,7 +34,7 @@ function App() {
             <Box display="flex" justifyContent="center" sx={{ p: 4 }}>
               <Typography variant="h4">Our Products</Typography>
             </Box>
-            <Products />
+            {/* <Products /> */}
             <Footer />
             <AppDrawer />
           </UIProvider>
